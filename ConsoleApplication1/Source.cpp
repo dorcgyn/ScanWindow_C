2e7f3ef537e7b243fd86c2c683b836c72f865dc9
dorcgyn <dorcgyn@gmail.com>
2015-06-08 09:43:38 -0400

    First useful

--- ConsoleApplication1/Source.cpp
+++ ConsoleApplication1/Source.cpp
@@ -7,16 +7,25 @@
 
 using namespace std;
 
-BOOL CALLBACK enumDesktopWindowsProc(__in HWND hWnd, __in  LPARAM lParam) {
-	int length = ::GetWindowTextLength(hWnd);
-	if (0==length) return true;
+int i = 0;
 
-	TCHAR* buffer;
-	buffer = new TCHAR[length+1];
+TCHAR* printWindowText (HWND hWnd) {
+	int length = ::GetWindowTextLength(hWnd);
+	if (0==length) return NULL;
+	
+	TCHAR* buffer = new TCHAR[length+1];
 	memset( buffer, 0, (length+1)*sizeof(TCHAR));
 	
 	GetWindowText( hWnd, buffer, length + 1 );
-    wcout <<buffer<<endl;
+    // wcout << buffer << endl;
+	return buffer;
+}
+
+BOOL CALLBACK enumDesktopWindowsProc(__in HWND hWnd, __in  LPARAM lParam) {
+    
+	TCHAR* buffer = printWindowText(hWnd);
+	if (buffer == NULL) return TRUE;
+
     wstring s2 ( buffer );
     wstring s3 (L"Ning") ;
     std::string::size_type found = s2.find(s3);
@@ -26,48 +35,76 @@ BOOL CALLBACK enumDesktopWindowsProc(__in HWND hWnd, __in  LPARAM lParam) {
 		HWND child = ::GetWindow(hWnd, GW_CHILD);
 		cout << "Child : ";
 
-		length = ::GetWindowTextLength(child);
-		if (0!=length) {
-		TCHAR* buffer2;
-		buffer2 = new TCHAR[length+1];
-		memset( buffer2, 0, (length+1)*sizeof(TCHAR));
-	
-		GetWindowText( child, buffer2, length + 1 );
-		wcout <<buffer2<<endl;
-		}
+		printWindowText(hWnd);
     }
     delete[] buffer;
     return TRUE;
 }
 
 BOOL CALLBACK enumWindowsProc(__in HWND hWnd, __in  LPARAM lParam) {
-	int length = ::GetWindowTextLength(hWnd);
-	if (0==length) return true;
+	i++;
+	TCHAR* buffer = printWindowText(hWnd);
+	if (buffer == NULL) return TRUE;
 
-	TCHAR* buffer;
-	buffer = new TCHAR[length+1];
-	memset( buffer, 0, (length+1)*sizeof(TCHAR));
-	
-	GetWindowText( hWnd, buffer, length + 1 );
-    wcout <<buffer<<endl;
     wstring s2 ( buffer );
-    wstring s3 (L"Ning") ;
+    // wstring s3 (L"Yu (HP") ;
+	wstring s3 (L"Tan, Chris") ;
     std::string::size_type found = s2.find(s3);
+	
     if (found!=std::string::npos)
     {
       // SendMessage(hWnd,WM_DESTROY,0,0);
-		HWND child = ::GetWindow(hWnd, GW_CHILD);
-		cout << "Child : ";
+		HWND ancestor = ::GetAncestor(hWnd, GA_ROOTOWNER);
 
-		length = ::GetWindowTextLength(child);
-		if (0!=length) {
-		TCHAR* buffer2;
-		buffer2 = new TCHAR[length+1];
-		memset( buffer2, 0, (length+1)*sizeof(TCHAR));
-	
-		GetWindowText( child, buffer2, length + 1 );
-		wcout <<buffer2<<endl;
-		}
+		// cout << "Ancentor: ";
+		printWindowText(ancestor);
+
+		// cout << "Window INof:  " << endl;
+		WINDOWINFO winInfo;
+		GetWindowInfo(hWnd, &winInfo);
+		// cout << winInfo.rcWindow.left << "; top " << winInfo.rcWindow.top << "; right " << winInfo.rcWindow.right << "; bottom " << winInfo.rcWindow.bottom << endl;
+
+		RECT rect;
+		GetWindowRect(hWnd, &rect);
+//		cout << rect.left << "; top " << rect.top << "; right " << rect.right << "; bottom " << rect.bottom << endl;
+
+	//	cout << "Is visibal: ";
+		 if (!IsWindowVisible(hWnd)) {
+			ShowWindow(hWnd, SW_SHOWNORMAL);
+			SetFocus(hWnd);
+			EnableWindow(hWnd, TRUE);
+			SetForegroundWindow(hWnd);
+
+			Sleep(100);
+
+			INPUT ip;
+			ip.type = INPUT_KEYBOARD;
+			ip.ki.wScan = 0; // hardware scan code for key
+			ip.ki.time = 0;
+			ip.ki.dwExtraInfo = 0;
+ 
+			// Press the "A" key
+			ip.ki.wVk = 0x41; // virtual-key code for the "a" key
+			ip.ki.dwFlags = 0; // 0 for key press
+			SendInput(1, &ip, sizeof(INPUT));
+			// Release the "A" key
+		    ip.ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP for key release
+		    SendInput(1, &ip, sizeof(INPUT));
+
+			// cout << "input a" << endl;
+			
+			INPUT ip2;
+			ip2.type = INPUT_KEYBOARD;
+			ip2.ki.wVk = 0x0D; // virtual-key code for the "Enter" key
+			ip2.ki.dwFlags = 0; // 0 for key press
+			SendInput(1, &ip2, sizeof(INPUT));
+			// Release the "Enter" key
+			ip2.ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP for key release
+			SendInput(1, &ip2, sizeof(INPUT));
+			
+			Sleep(1000);
+		 	SendMessage(hWnd, WM_CLOSE, 0, 0);
+		 }
     }
     delete[] buffer;
     return TRUE;
@@ -84,10 +121,10 @@ int main(int argc, char* argv[])
 	std::cout << "Hellow world" << endl;
 
 	// Get current thread
-	DWORD dwThreadId = GetCurrentThreadId();
-	HDESK hDesktop = GetThreadDesktop(dwThreadId);
-	BOOL enumeratingWindowsSucceeded = ::EnumDesktopWindows(hDesktop, enumDesktopWindowsProc, NULL);
-	std::cout << "Hellow world 3" << endl;
-	BOOL enumeratingWindowsSucceeded2 = ::EnumWindows(enumWindowsProc, NULL);
+	// DWORD dwThreadId = GetCurrentThreadId();
+	while (true) { 
+		BOOL enumeratingWindowsSucceeded2 = ::EnumWindows(enumWindowsProc, NULL);
+		Sleep(3000);
+	}
 	return 0;
 }
